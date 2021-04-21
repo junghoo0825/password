8db0ae5a2554173c4ccdc5dc97193fdb6ac52123
junghoo0825 <81668830+junghoo0825@users.noreply.github.com>
2021-04-21 14:52:51 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onButtonPressed(Button.A, function () {
     _0자리수 += 1
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    리스트 = [0, 0, 0, 0]
 })
 input.onButtonPressed(Button.B, function () {
     리스트.insertAt(_0자리수, _0)
@@ -14,29 +14,25 @@ let _0자리수 = 0
 let _1 = 0
 let _0 = 0
 pins.digitalWritePin(DigitalPin.P16, 0)
-let 비밀 = 2
 _0자리수 = 1
 _0 = 0
 리스트 = [0, 0, 0, 0]
 _1 = 1
 basic.forever(function () {
-    if (비밀 == 1) {
-        pins.digitalWritePin(DigitalPin.P16, 1)
-    }
-})
-basic.forever(function () {
-    if (_0자리수 == 5) {
+    if (리스트.length == 4) {
         if (리스트[1] == 1) {
             if (리스트[2] == 0) {
                 if (리스트[3] == 1) {
                     if (리스트[4] == 0) {
-                        basic.showIcon(IconNames.Yes)
-                        비밀 = 1
+                        pins.digitalWritePin(DigitalPin.P16, 1)
                     }
                 }
             }
         }
     }
+})
+basic.forever(function () {
+	
 })
 basic.forever(function () {
 	
